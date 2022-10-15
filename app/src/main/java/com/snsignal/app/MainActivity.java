0685d339be9ddb9c4482cc733fe9db901aa3d0f7
cherot <hectornovoadita2000@gmail.com>
2022-10-15 14:29:11 -0500

    First commit. Desconociendo si el valor marcado corresponde al RSSI o algún otro valor.

---
+++ app/src/main/java/com/snsignal/app/MainActivity.java
@@ -0,0 +1,75 @@
+package com.snsignal.app;
+
+
+import static android.os.SystemClock.sleep;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.rssi);
+        //while(true){
+        //    sleep(3000);
+        textView.setText(""+getSignalStrength(this));
+        //}
+
+    }
+
+    private static String getSignalStrength(Context context) throws SecurityException {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String strength = null;
+        List<CellInfo> cellInfos = telephonyManager.getAllCellInfo();   //This will give info of all sims present inside your mobile
+        if(cellInfos != null) {
+            for (int i = 0 ; i < cellInfos.size() ; i++) {
+                if (cellInfos.get(i).isRegistered()) {
+                    if (cellInfos.get(i) instanceof CellInfoWcdma) {
+                        CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfos.get(i);
+                        CellSignalStrengthWcdma cellSignalStrengthWcdma = cellInfoWcdma.getCellSignalStrength();
+                        strength = String.valueOf(cellSignalStrengthWcdma.getDbm());
+                    } else if (cellInfos.get(i) instanceof CellInfoGsm) {
+                        CellInfoGsm cellInfogsm = (CellInfoGsm) cellInfos.get(i);
+                        CellSignalStrengthGsm cellSignalStrengthGsm = cellInfogsm.getCellSignalStrength();
+                        strength = String.valueOf(cellSignalStrengthGsm.getDbm());
+                    } else if (cellInfos.get(i) instanceof CellInfoLte) {
+                        CellInfoLte cellInfoLte = (CellInfoLte) cellInfos.get(i);
+                        CellSignalStrengthLte cellSignalStrengthLte = cellInfoLte.getCellSignalStrength();
+                        strength = String.valueOf(cellSignalStrengthLte.getDbm());
+                    } else if (cellInfos.get(i) instanceof CellInfoCdma) {
+                        CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfos.get(i);
+                        CellSignalStrengthCdma cellSignalStrengthCdma = cellInfoCdma.getCellSignalStrength();
+                        strength = String.valueOf(cellSignalStrengthCdma.getDbm());
+                    }
+                }
+            }
+        }
+        return strength;
+    }
+}
