a37ae64d60228cc900b824fb394c8a73f540e801
cherot <hectornovoadita2000@gmail.com>
2022-10-15 23:57:09 -0500

    Adding Timed RSSI update

--- app/src/main/java/com/snsignal/app/MainActivity.java
+++ app/src/main/java/com/snsignal/app/MainActivity.java
@@ -1,12 +1,13 @@
 package com.snsignal.app;
 
 
-import static android.os.SystemClock.sleep;
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.READ_PHONE_NUMBERS;
+import static android.Manifest.permission.READ_PHONE_STATE;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ import android.telephony.CellSignalStrengthGsm;
 import android.telephony.CellSignalStrengthLte;
 import android.telephony.CellSignalStrengthWcdma;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.List;
@@ -28,22 +30,24 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private TextView typeNetwork;
 
-    @SuppressLint("SetTextI18n")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission();
 
         textView = findViewById(R.id.rssi);
-        //while(true){
-        //    sleep(3000);
+        typeNetwork = findViewById(R.id.networkType);
+
         textView.setText(""+getSignalStrength(this));
-        //}
+
 
     }
 
-    private static String getSignalStrength(Context context) throws SecurityException {
+    public String getSignalStrength(Context context) throws SecurityException {
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         String strength = null;
         List<CellInfo> cellInfos = telephonyManager.getAllCellInfo();   //This will give info of all sims present inside your mobile
@@ -53,23 +57,64 @@ public class MainActivity extends AppCompatActivity {
                     if (cellInfos.get(i) instanceof CellInfoWcdma) {
                         CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfos.get(i);
                         CellSignalStrengthWcdma cellSignalStrengthWcdma = cellInfoWcdma.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthWcdma.getDbm());
+                        strength = String.valueOf(cellSignalStrengthWcdma.getDbm()) + " dBm";
+                        typeNetwork.setText("Tipo de RED: 3G (WCDMA)");
+
                     } else if (cellInfos.get(i) instanceof CellInfoGsm) {
                         CellInfoGsm cellInfogsm = (CellInfoGsm) cellInfos.get(i);
                         CellSignalStrengthGsm cellSignalStrengthGsm = cellInfogsm.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthGsm.getDbm());
+                        strength = String.valueOf(cellSignalStrengthGsm.getDbm()) + " dBm";
+                        typeNetwork.setText("Tipo de RED: 2G (GSM)");
+
                     } else if (cellInfos.get(i) instanceof CellInfoLte) {
                         CellInfoLte cellInfoLte = (CellInfoLte) cellInfos.get(i);
                         CellSignalStrengthLte cellSignalStrengthLte = cellInfoLte.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthLte.getDbm());
+                        strength = String.valueOf(cellSignalStrengthLte.getDbm()) + " dBm";
+                        typeNetwork.setText("Tipo de RED: 4G (LTE)");
+
                     } else if (cellInfos.get(i) instanceof CellInfoCdma) {
                         CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfos.get(i);
                         CellSignalStrengthCdma cellSignalStrengthCdma = cellInfoCdma.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthCdma.getDbm());
+                        strength = String.valueOf(cellSignalStrengthCdma.getDbm()) + " dBm";
+                        typeNetwork.setText("Tipo de RED: 3G (CDMA)");
+
                     }
                 }
             }
         }
         return strength;
+    }
+
+    private class getTimedSignalStrength implements Runnable{
+
+        private boolean stopRequested = false;
+
+        public synchronized boolean isStopRequested() {
+            return this.stopRequested;
+        }
+
+
+        private void sleep(long millis) {
+            try {
+                Thread.sleep(millis);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            Log.d("myTag","Actuaizando RSSI");
+            while (!isStopRequested()) {
+                sleep(5000);
+
+            }
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION, READ_PHONE_NUMBERS, READ_PHONE_STATE}, 100);
+        }
     }
 }
