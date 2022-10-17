aa7299ac65cd5bc4d6f48f06d8f0ac49018ed11a
cherot <hectornovoadita2000@gmail.com>
2022-10-16 19:30:39 -0500

    added location info

--- app/src/main/java/com/snsignal/app/MainActivity.java
+++ app/src/main/java/com/snsignal/app/MainActivity.java
@@ -6,9 +6,16 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.READ_PHONE_NUMBERS;
 import static android.Manifest.permission.READ_PHONE_STATE;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.security.AccessController.getContext;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.telephony.CellInfo;
@@ -30,8 +37,8 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private TextView typeNetwork;
+    private TextView textView, typeNetwork, tvLatitude, tvLongitude;
+    private LocationManager locationManager;
 
 
     @Override
@@ -42,16 +49,21 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.rssi);
         typeNetwork = findViewById(R.id.networkType);
-        textView.setText(""+getSignalStrength(this));
+        tvLatitude = findViewById(R.id.latitude);
+        tvLongitude = findViewById(R.id.longitude);
 
-        MultiThreadingTest object =  new MultiThreadingTest(this);
-        object.start();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        getSignalStrength(this);
+        GetLatLon();
+        /*MultiThreadingTest object =  new MultiThreadingTest(this);
+        object.start();*/
 
 
     }
 
 
-    class MultiThreadingTest extends Thread{
+    /*class MultiThreadingTest extends Thread{
 
         Context context;
 
@@ -79,13 +91,12 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-    }
+    }*/
 
 
-
-    public String getSignalStrength(Context context) throws SecurityException {
+    public void getSignalStrength(Context context) throws SecurityException {
         TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        String strength = null;
+        //String strength = null;
         List<CellInfo> cellInfos = telephonyManager.getAllCellInfo();   //This will give info of all sims present inside your mobile
         if(cellInfos != null) {
             for (int i = 0 ; i < cellInfos.size() ; i++) {
@@ -93,36 +104,62 @@ public class MainActivity extends AppCompatActivity {
                     if (cellInfos.get(i) instanceof CellInfoWcdma) {
                         CellInfoWcdma cellInfoWcdma = (CellInfoWcdma) cellInfos.get(i);
                         CellSignalStrengthWcdma cellSignalStrengthWcdma = cellInfoWcdma.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthWcdma.getDbm()) + " dBm";
-                        typeNetwork.setText("Tipo de RED: 3G (WCDMA)");
+                        //strength = String.valueOf(cellSignalStrengthWcdma.getDbm()) + " dBm";
+                        textView.setText(cellSignalStrengthWcdma.getDbm() + " dBm");
+                        typeNetwork.setText("3G (WCDMA)");
                         break;
 
                     } else if (cellInfos.get(i) instanceof CellInfoGsm) {
                         CellInfoGsm cellInfogsm = (CellInfoGsm) cellInfos.get(i);
                         CellSignalStrengthGsm cellSignalStrengthGsm = cellInfogsm.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthGsm.getDbm()) + " dBm";
-                        typeNetwork.setText("Tipo de RED: 2G (GSM)");
+                        //strength = String.valueOf(cellSignalStrengthGsm.getDbm()) + " dBm";
+                        textView.setText(cellSignalStrengthGsm.getDbm() + " dBm");
+                        typeNetwork.setText("2G (GSM)");
                         break;
 
                     } else if (cellInfos.get(i) instanceof CellInfoLte) {
                         CellInfoLte cellInfoLte = (CellInfoLte) cellInfos.get(i);
                         CellSignalStrengthLte cellSignalStrengthLte = cellInfoLte.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthLte.getDbm()) + " dBm";
-                        typeNetwork.setText("Tipo de RED: 4G (LTE)");
+                        //strength = String.valueOf(cellSignalStrengthLte.getDbm()) + " dBm";
+                        textView.setText(cellSignalStrengthLte.getDbm() + " dBm");
+                        typeNetwork.setText("4G (LTE)");
                         break;
 
                     } else if (cellInfos.get(i) instanceof CellInfoCdma) {
                         CellInfoCdma cellInfoCdma = (CellInfoCdma) cellInfos.get(i);
                         CellSignalStrengthCdma cellSignalStrengthCdma = cellInfoCdma.getCellSignalStrength();
-                        strength = String.valueOf(cellSignalStrengthCdma.getDbm()) + " dBm";
-                        typeNetwork.setText("Tipo de RED: 3G (CDMA)");
+                        //strength = String.valueOf(cellSignalStrengthCdma.getDbm()) + " dBm";
+                        textView.setText(cellSignalStrengthCdma.getDbm() + " dBm");
+                        typeNetwork.setText("3G (CDMA)");
                         break;
 
                     }
                 }
             }
         }
-        return strength;
+    }
+
+    public void GetLatLon() {
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+
+                tvLatitude.setText("" + location.getLatitude());
+                tvLongitude.setText("" + location.getLongitude());
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+
+        };
+        // Register the listener with the location manager to receive location updates
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
     private void requestPermission() {
